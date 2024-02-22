4c1c000cbc535bbe97ad28585d49bbd5ca1d7832
Akkuman <akkumans@qq.com>
2024-02-22 11:00:33 +0800

    chore: 优化代码

--- tasks.py
+++ tasks.py
@@ -6,14 +6,15 @@ from config import redis_url
 
 app = Celery('tasks', broker=redis_url)
 
+
+queue_broadcast_tasks = Broadcast('broadcast_tasks', exchange=Exchange('broadcast_tasks', type='fanout'), routing_key='celery')
 # task_queues 此处必须要定义
-app.conf.task_queues = (Broadcast('broadcast_tasks', exchange=Exchange('broadcast_tasks', type='fanout'), routing_key='celery'),)
+app.conf.task_queues = (queue_broadcast_tasks,)
 app.conf.task_routes = {
     'tasks.broadcast_fn': {
-        'queue': 'broadcast_tasks',
-        'exchange': 'broadcast_tasks',
-        # 'exchange': Exchange('broadcast_tasks', type='fanout'),
-        'routing_key': 'celery'
+        'queue': queue_broadcast_tasks.name,
+        'exchange': queue_broadcast_tasks.exchange,
+        'routing_key': queue_broadcast_tasks.routing_key
     }
 }
 app.conf.worker_pool = 'solo'
